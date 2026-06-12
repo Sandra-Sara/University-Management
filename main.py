66c73f80809f08ed860e999e7a670ce844e5413b
Im Sara <sarafaria-2022315933@cs.du.ac.bd>
2026-06-12 14:13:54 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from database.mongodb import mongodb
+from routers.student import router as student_router
+from routers.staff import router as staff_router
+from routers.teacher import router as teacher_router
+
+app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(student_router)
+app.include_router(staff_router)
+app.include_router(teacher_router)
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    mongodb.close()
+
+@app.get("/")
+def role_select_page():
+    return FileResponse("templates/role_select.html")
+
+
+@app.get("/student")
+def student_login_page():
+    return FileResponse("templates/student_login.html")
+
+
+@app.get("/teacher")
+def teacher_login_page():
+    return FileResponse("templates/teacher_login.html")
+
+
+@app.get("/staff")
+def staff_login_page():
+    return FileResponse("templates/staff_login.html")
+
+
+@app.get("/register")
+def register_page():
+    return FileResponse("templates/student_createaccount.html")
+
+
+@app.get("/teacher-register")
+def teacher_register_page():
+    return FileResponse("templates/teacher_createaccount.html")
+
+
+@app.get("/staff-register")
+def staff_register_page():
+    return FileResponse("templates/staff_createaccount.html")
+
+# Optional dashboard test route
+@app.get("/dashboard")
+def dashboard():
+    return {"message": "Dashboard Working"}
